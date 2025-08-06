e1b93dcaf79e0fa8c216c11b55ff73705011aad3
gradient-pulse/phi-mesh repository <gradientpulse@proton.me>
2025-08-06 17:26:56 +0200

    Update link_index.js

--- docs/link_index.js
+++ docs/link_index.js
@@ -11,3 +11,28 @@ const linkIndex = {
     podcasts: ["mesh_academy/curriculum/recursive_gradients.md"]
   }
 };
+function displayLinks(tag) {
+  const links = linkIndex[tag];
+  if (!links) return;
+
+  const sidebar = document.getElementById("sidebar-content");
+  sidebar.innerHTML = "";
+
+  if (links.pulses && links.pulses.length > 0) {
+    sidebar.innerHTML += "<h4>Pulses</h4><ul>" +
+      links.pulses.map(p => `<li><a href="/phi-mesh/${p}" target="_blank">${p}</a></li>`).join("") +
+      "</ul>";
+  }
+
+  if (links.papers && links.papers.length > 0) {
+    sidebar.innerHTML += "<h4>Papers</h4><ul>" +
+      links.papers.map(p => `<li><a href="/phi-mesh/${p}" target="_blank">${p}</a></li>`).join("") +
+      "</ul>";
+  }
+
+  if (links.podcasts && links.podcasts.length > 0) {
+    sidebar.innerHTML += "<h4>Podcasts</h4><ul>" +
+      links.podcasts.map(p => `<li><a href="/phi-mesh/${p}" target="_blank">${p}</a></li>`).join("") +
+      "</ul>";
+  }
+}
